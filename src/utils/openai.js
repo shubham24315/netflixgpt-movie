97028b26f0d12341d75c1d0f1cb47e1e1bd5d9b1
Shubham <gondshubham786@gmail.com>
2026-04-03 17:15:57 +0530

    changes made to add deepseek model

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,8 +1,9 @@
 import OpenAI from "openai";
-import { OPENAI_KEY } from "./constants";
-
+import { DEEPSEEK_KEY } from "./constants";
+console.log("My API Key is:", DEEPSEEK_KEY); // This will likely print: "My API Key is: undefined"
 const openai = new OpenAI({
-  apiKey: OPENAI_KEY,
+  baseURL: "https://api.deepseek.com/v1",
+  apiKey: DEEPSEEK_KEY,
   dangerouslyAllowBrowser: true,
 });
 
